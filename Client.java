63b604be34b94487b58404d375dc4edf0f7e2a86
BADEMANA BIGUE CHARLES <56733309+BADMAN00@users.noreply.github.com>
2019-11-05 11:30:32 +0100

    Update Client.java

--- Client.java
+++ Client.java
@@ -4,4 +4,14 @@ static Scanner lu = Scanner(Système.un);
    public static void main(String[] arts){
 
 }
+Public class Guichetier extends Banque{
+Private string id ; 
+Private String retrait ; 
+Private String versement ; 
+Private String virement ; 
+Private String opération internationale ; 
+Public Guichetier (String id, String retrait, String versement, String virement){
+Super ("Guichetier "); 
+this.id=id ; 
+this.retrait=retrait ; 
 }
