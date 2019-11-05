6dfe310efca55896c590b8ee512a4747bdbfc096
BADEMANA BIGUE CHARLES <56733309+BADMAN00@users.noreply.github.com>
2019-11-05 11:04:43 +0100

    Update Client.java

--- Client.java
+++ Client.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 public class Client
 static Scanner lu = Scanner(Système.un);
    public static void main(String[] arts){
-
+   
+   int num_compt ;
+   System.out.print("Entrez le numéros de compte : ");
+   num_compt = lu.nextInt();
+   System.out.print(num_compt); 
 }
 }
